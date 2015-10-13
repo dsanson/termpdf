3d20f6e7979f5212108781d43c0e089c418f6525
David Sanson <dsanson@gmail.com>
2015-10-12 22:02:43 -0500

    make it actually work for most pdfs (by using gs)

--- README.md
+++ README.md
@@ -1,11 +1,24 @@
 # README for termpdf
 
-`termpdf` is a barebones pdf viewer that relies on [iTerm2's support of inline
-images][] and the [Poppler PDF rendering library][]. It should run fine within
-tmux. I wrote it so that I could view PDF files while working in tmux and
-iTerm2 in fullscreen mode.
+`termpdf` is a barebones inline terminal pdf viewer that relies on [iTerm2's
+support of inline images][].
 
-It it not especially robust, but it works well enough that I find it useful.
+It works by converting each page of the pdf into a png, using Ghostscript.
+If a pdf happens to already contain one image for each page, it
+instead uses [Poppler][]'s `pdfimages` to extract those images.
+
+It automatically sizes pages to fit within the terminal window (or, within
+`tmux`, the pane).
+It does not support zooming, but it does support autocropping of margins,
+for which it relies on ImageMagick's `convert -trim` command.
+It also supports text search, using `pdfgrep`, and a rudimentary paginated
+text view, that relies on [Poppler][]'s `pdftotext`.
+
+I wrote `termpdf` so that I could view PDF files while working
+in tmux and iTerm2 in fullscreen mode.
+
+It is not especially robust. In `tmux` I find that the image of the PDF often
+goes missing. But pressing `r` usually suffices to bring it back.
 
 ![screenshot](termpdf_screenshot.png)
 
@@ -14,13 +27,26 @@ It it not especially robust, but it works well enough that I find it useful.
 `termpdf` is a bash script. Put it somewhere in your path and make sure it has
 the appropriate permissions (i.e., `chmod u+x termpdf.sh`).
 
-You can install Poppler via homebrew: `brew install poppler`.
+Dependencies: 
+
+-   Ghostscript (`gs`), ImageMagick (`convert`), Poppler
+(`pdftotext`, `pdfimages`, and `pdfinfo`), and `pdfgrep`
+
+To install these view homebrew, try
+
+```
+brew install gs imagemagick poppler pdfgrep
+```
+
+If you don't need autocropped margins, you can easily hack the script
+to remove the ImageMagick dependency. 
+
 
 # Usage
 
 From the command line, try,
 
-`$ termpdf.sh [options] sample.pdf`
+`$ termpdf.sh [options] file.pdf`
 
 options: 
 
@@ -34,11 +60,14 @@ While viewing a PDF, the commands are:
    j/k:         page back/forward
    g <number>:  go to page number
    r:           resize and redraw to fit pane
+   m:           toggle autocropped margins
    t:           toggle text/image display
    y:           yank current page as text to clipboard
    / <expr>     go to page with first match for <expr>
    n:           go to next match for <expr>
    h:           print this help
+   p:           switch to between pagers in text mode
+   w:           wrap lines in text mode
    q:           quit
 ~~~
 
--- termpdf.sh
+++ termpdf.sh
@@ -1,25 +1,46 @@
-#!/bin/sh
-#
+#!/usr/bin/env bash
+###########################################################################
 # termpdf is a barebones pdf viewer that relies on iTerm2's 
 # support of inline images:
 # 
 #   https://iterm2.com/images.html
 #
-# and tools provided by the Poppler PDF rendering library
+# To render the pdf into images, we rely on ghostscript and
+# poppler's pdfimages command. To autocrop margins, we rely
+# on ImageMagick's convert command. For search, we rely on
+# pdfgrep.
 #
-#   http://poppler.freedesktop.org/
+# You can install ghostscript, poppler, ImageMagick, and pdfgrep
+# via homebrew:
 #
-# You can install Poppler via homebrew:
+#   brew install poppler gs imagemagick pdfgrep
 #
-#   brew install poppler
+# termpdf should work inside of tmux.
 #
-# It should work inside of tmux.
-#
+###########################################################################
 
-# The following functions are borrowed from the imgcat script at
+
+# SETTINGS
+
+display="image" # default is to display images not text
+wrap="false" # default is not to pipe text through wrap in text mode 
+# default pager is cat. secondary pager is less: a lot more power, but
+# clunky integration with termpdf. 
+text_pagers[0]="cat"
+text_pagers[1]="less -XE"
+# density for PDF conversion. Higher density means sharper images,
+# but might lead to slower performance.
+density=200 
+# halo is the number of pages before and after the current page that will
+# be converted in the background.
+halo=2
+
+
+#
+# The following two functions are borrowed from the imgcat script at
 # 
 #   https://raw.githubusercontent.com/gnachman/iTerm2/master/tests/imgcat
-
+#
 # tmux requires unrecognized OSC sequences to be wrapped with DCS tmux;
 # <sequence> ST, and for all ESCs in <sequence> to be replaced with ESC ESC. It
 # only accepts ESC backslash for ST.
@@ -31,7 +52,6 @@ function print_osc() {
     fi
 }
 
-# More of the tmux workaround described above.
 function print_st() {
     if [[ $TERM == screen* ]] ; then
         printf "\a\033\\"
@@ -40,6 +60,9 @@ function print_st() {
     fi
 }
 
+# This function is also borrowed from imgcat, but I've added support
+# for specifying width and height.
+#
 # print_image filename inline base64contents
 #   filename: Filename to convey to client
 #   inline: 0 or 1
@@ -66,6 +89,7 @@ function print_image() {
     printf '\n'
 }
 
+# A simple function that uses pdfgrep to search the pdf
 function find_text() {
   read -p "Find: " text
   results=( $(pdfgrep -nip "$text" "$pdf_file" | sed 's|:.*||') )
@@ -76,6 +100,8 @@ function find_text() {
   fi
 }
 
+# A function for getting the dimensions of the current terminal
+# window or pane.
 function get_pane_size() {
     width=$(tput cols)
     height=$(stty size | awk '{print $1}')
@@ -83,10 +109,70 @@ function get_pane_size() {
     height=$(expr $height - 1 )
 }
 
+# A function for validiating page numbers
 function page_limits() {
    if [ "$n" -le 0 ]; then n=1; fi
    if [ "$n" -ge "$pages" ]; then n="$pages"; fi
 }
+
+# Convert a page of the pdf to png. Using gs rather than 
+# convert because it is supposedly faster. Also means that
+# if you don't use the margin trimming feature, you don't need
+# imagemagick.
+function convert_gs() {
+   gs -dNumRenderingThreads=4 -dNOPAUSE -sDEVICE=png16m \
+       -dFirstPage=$1 -dLastPage=$1 \
+       -sOutputFile=${tmp_file_root}-$1.png -r$density \
+       -q "${pdf_file}" -c quit 2>/dev/null
+}
+
+# 
+function convert_pdf() {
+    for i in "$@"
+    do
+        if [[ $i -le $pages && $i -ge 1 ]]; then
+            if [[ ! -f "${tmp_file_root}-$i.png" ]]; then
+                if [[ $use_images == 'false' ]]; then
+                    convert_gs "$i"
+                else
+                    pdfimages -png -f $i -l $i "$pdf_file" "$tmp_file_root"
+                    mv "$tmp_file_root-000.png" "$tmp_file_root-$i.png"
+                fi
+                # generate margin-trimmed version of the page
+                convert -trim "$tmp_file_root-$i.png" "$tmp_file_root-trimmed-$i.png" 2>/dev/null
+            fi
+        fi
+    done
+}
+
+function convert_pdf_background() {
+   (
+   core=$n
+   k=1
+   while ((k<=halo))
+   do
+      convert_pdf $[$core + $k] $[$core - $k]
+      let k++
+   done
+   )&
+}
+
+function display_text() {
+   clear
+   pdftotext -f $n -l $n -layout "$pdf_file" - \
+       | egrep --color "$text|\$" \
+       | if [ $wrap == 'true' ]; then wrap -w $width; else cat; fi \
+       | $text_pager 
+   if [[ $text_pager != 'cat' ]]; then echo "---END OF PAGE---"; fi
+}
+
+function check_dependencies() {
+   for app in gs pdftotext pdfimages pdfgrep pdfinfo convert 
+   do
+      command -v $app >/dev/null 2>&1 ||\
+          { echo >&2 "I require $app but it's not installed."; exit 1; } 
+   done
+   }
 
 function print_help() {
    clear
@@ -106,27 +192,30 @@ function print_help() {
 }
 
 function cli_help() {
-   echo "Usage: termpdf.sh [options] path_to_pdf"
+   echo "Usage: termpdf.sh [options] file.pdf"
    echo
    echo "   options:"
-   echo "      -h|--help: show this help"
-   echo "      -t|--text: display text instead of images"
+   echo "      -h|--help:   show this help"
+   echo "      -t|--text:   display text instead of images"
    echo "      -n <int>:    display page number <n>" 
-   echo 
-   echo "   dependencies:"
-   echo "       iTerm2 and poppler (for pdfimages, pdftotext, and pdfinfo)"
    exit
 }
 
-# Default settings
+function requires_poppler() {
+    echo "termpdf requires ghostscript and poppler." 
+    exit
+}
+
+check_dependencies
+
+# Set some defaults
 n=1 # start on page 1
-display="image" # display images not text
-
-
-# No search results yet
+text_pager=${text_pagers[0]}
 results=( )
 index=-1 
-text="$"
+text="$" # null search text for egrep
+trimmed="false" # set to true to default to trim margins
+
 
 # Look for command line flags
 while [ $# -gt 0 ]; do
@@ -164,11 +253,16 @@ while [ $# -gt 0 ]; do
     shift
 done
 
+
 # Check to see that a file was specified on the cli
-if [ ! $pdf_file ]; then cli_help; fi
+if [ ! -r "$pdf_file" ]; then cli_help; fi
 
 # How many pages does the PDF file have?
-pages=$(pdfinfo "$pdf_file" | grep "^Pages:" | awk '{print $2}') 
+pages=$(pdfinfo "$pdf_file" 2>/dev/null | grep "^Pages:" | awk '{print $2}') 
+# Does it look like it has page images?
+images=$(pdfimages -list "$pdf_file" | wc | awk '{print $1}')
+images=$[$images - 2]
+if [[ $pages == $images ]]; then use_images='true'; else use_images='false'; fi
 
 # Ensure that $n is set to a page that exists
 page_limits
@@ -178,32 +272,46 @@ page_limits
 get_pane_size
 
 # Make a tmpfile
-tmp_file_root=$(mktemp)
-tmp_file="${tmp_file_root}-000.png"
-
+tmp_dir=$(mktemp -d)
+tmp_file_root="${tmp_dir}/tmp"
 # clear the pane since we don't always fill it
 clear
 
+# Convert page $n of the PDF
+convert_pdf $n
+
+# Convert surrounding pages in the background 
+convert_pdf_background 
+
 # display the PDF
-while $(pdfimages -png -f $n -l $n "$pdf_file" "$tmp_file_root")  
+
+while [[ 1 == 1 ]]
 do
    tput cup 0 1  # we leave a line at the top for commands
 
+   if [[ $trimmed == 'true' ]]; then
+       tmp_file="${tmp_file_root}-trimmed-$n.png"
+   else
+       tmp_file="${tmp_file_root}-$n.png"
+   fi
    # display
    if [ $display == 'image' ]; then
        if [ -r "$tmp_file" ] ; then
            print_image "$tmp_file" 1 "$(base64 < "$tmp_file")"
        else
-           error "termpdf: unable to create the temporary file"
-           exit 2
+           convert_pdf $n
+           print_image "$tmp_file" 1 "$(base64 < "$tmp_file")"
        fi
    else
-       clear
-       pdftotext -f $n -l $n "$pdf_file" - | egrep --color "$text|\$"
+       display_text $n
    fi
 
    tput cup 0 0 # put the cursor at the top of the pane
    tput el # erase any old stuff from previous commands
+   tput cup 0 $(expr $width - ${#n}) # page num top right
+   echo "$n"
+   tput cup 0 0
+
    read -n 1 -s command # await commands
    case "$command" in
       j)
@@ -228,8 +336,29 @@ do
           fi
           clear
           ;;
+      m)
+          if [ $trimmed == "false" ]; then
+              trimmed="true"
+          else
+              trimmed="false"
+          fi
+          ;;
+      p)
+          if [[ "$text_pager" == "${text_pagers[1]}" ]]; then
+              text_pager="${text_pagers[0]}"
+          else
+              text_pager="${text_pagers[1]}"
+          fi
+          ;;
+       w)
+          if [ $wrap == "true" ]; then
+              wrap="false"
+          else
+              wrap="true"
+          fi
+          ;;
       y)
-          pdftotext -f $n -l $n "$pdf_file" - | pbcopy
+          pdftotext -f $n -l $n -layout "$pdf_file" - | pbcopy
           tput cup 0 0
           echo "Page copied"
           ;;
@@ -251,9 +380,11 @@ do
           print_help
           ;;
       q)
-          rm "$tmp_file"
+          rm -rf "$tmp_dir"
+          clear
           exit;;
    esac
    # make sure the page we want exists
    page_limits
+   convert_pdf_background
 done
